97c6ba1aeb4be8dd8bc4d3b788b3b1ee461c9603
Kyounghoon-park <orangejooos@gmail.com>
2019-04-06 15:48:13 +0900

    first commit

--- web_crawl/samsung_2018.py
+++ web_crawl/samsung_2018.py
@@ -1,2 +1,20 @@
 from konlpy.tag import Okt
+import re
+
+#import nltk
+#nltk.download()
+from nltk.tokenize import word_tokenize
+
+ctx='../data/'
+filename=ctx+'kr-Report_2018.txt'
+with open(filename,'r',encoding='UTF-8')as f:
+    texts=f.read()
+print(texts[:300])
+
+texts=texts.replace('\n','')
+tokenizer=re.compile('[^ ㄱ.힣]+')
+texts= tokenizer.sub('',texts)
+
+tokens= word_tokenize(texts)
+print(texts[:300])
 
